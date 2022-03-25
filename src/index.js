8d71fb50d4d1616e2d37dcd7ac0807abb751a081
Ozymandias-9 <galac.9173@gmail.com>
2022-03-25 17:20:32 -0600

    changes on folders

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import Foro from "./Components/Foro/index.js";
-import User from "./Components/User/index.js";
-import Config from "./Components/User/config.js"
+import Foro from "./Pages/Foro/foro.js";
+import User from "./Pages/User/perfil.js";
+import Config from "./Pages/User/config.js"
 
 // import { AuthProvider } from './Contexts/AuthContext';
 // import { createTheme, ThemeProvider } from '@material-ui/core/styles';
@@ -26,12 +26,12 @@ ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Foro />} />
-      <Route path="inicio" element={<Foro />} />
+      <Route path="foros" element={<Foro />} />
       <Route path="listas" />
       <Route path="login" />
       <Route path="roadmap" />
-      <Route path="/perfil" element={<User />} />
-      <Route path="/config" element={<Config />} />
+      <Route path="perfil" element={<User />} />
+      <Route path="config" element={<Config />} />
       <Route path="*" />
     </Routes>
   </BrowserRouter>,
