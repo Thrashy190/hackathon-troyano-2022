f90e4cd719fb5ba1f7b184aff2bc0e3a9792d37f
Thrashy190 <diego456.dlm77@gmail.com>
2022-03-25 13:19:47 -0600

    login

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import App from "./App";
+import Login from "./Pages/Login/Login";
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />{" "}
+      <Route path="/" element={Login} />
       {/* Incluimos un elemento para renderizar con esa ruta */}
       <Route path="inicio" />
       <Route path="listas" />
       <Route path="login" />
       <Route path="roadmap" />
       <Route path="perfil" />
-      <Route path="*" />{" "}
+      <Route path="*" />
       {/*Esta ruta funciona para la captura de todo lo no definido en el Router */}
     </Routes>
   </BrowserRouter>,
