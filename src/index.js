e18f8fd879e984fd5df9d333fe8b8868060169f1
Ozymandias-9 <galac.9173@gmail.com>
2022-03-25 22:35:57 -0600

    firebase :(

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import { AuthProvider } from "./contexts/authContext";
+import { AuthProvider } from "./contexts/authContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "./index.css";
@@ -10,6 +10,35 @@ import Config from "./Pages/User/config.js";
 import Login from "./Pages/Login/Login";
 import Universidades from "./Pages/Listas/universidades.js"
 import NM from "./Pages/Otros/404.js";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore"
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAivt1eo738_e4s4tSdrvh8ovBhZcNMmK8",
+  authDomain: "hackathon-2022-b997c.firebaseapp.com",
+  databaseURL: "https://hackathon-2022-b997c-default-rtdb.firebaseio.com",
+  projectId: "hackathon-2022-b997c",
+  storageBucket: "hackathon-2022-b997c.appspot.com",
+  messagingSenderId: "217569159200",
+  appId: "1:217569159200:web:fded9663e1d4d46423b445",
+  measurementId: "G-VQ6H4JJRF5"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const ref = collection(db, 'users');
+
+const getUsers = async () => {
+  getDocs(ref);
+
+  addDoc(db, {carrera: 'lati18', email: 'ola', forumInteractions: 'ola', universidad: 'uam', userName: 'ola'});
+}
+
+getUsers();
+
+// console.log(getUsers());
 
 const theme = createTheme({
   palette: {
@@ -26,7 +55,9 @@ const theme = createTheme({
   },
 });
 
+
 ReactDOM.render(
+  // <AuthProvider>
   <ThemeProvider theme={theme}>
     <BrowserRouter>
       <Routes>
@@ -42,5 +73,6 @@ ReactDOM.render(
       </Routes>
     </BrowserRouter>
   </ThemeProvider>,
+  // </AuthProvider>,
   document.getElementById("root")
 );
--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -1,21 +1,37 @@
-import firebase from "./firebase";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from 'firebase/firestore/lite';
+// import { getAuth } from "firebase/auth";
+// import { getStorage } from "firebase/storage";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAivt1eo738_e4s4tSdrvh8ovBhZcNMmK8",
+//   authDomain: "hackathon-2022-b997c.firebaseapp.com",
+//   projectId: "hackathon-2022-b997c",
+//   storageBucket: "hackathon-2022-b997c.appspot.com",
+//   messagingSenderId: "217569159200",
+//   appId: "1:217569159200:web:fded9663e1d4d46423b445",
+//   measurementId: "G-VQ6H4JJRF5",
+// };
+
+// const app = initializeApp(firebaseConfig);
+
+// export const auth = getAuth();
+
+// export const db = getFirestore(app);
+
+// export const store = getStorage(app);
+
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAivt1eo738_e4s4tSdrvh8ovBhZcNMmK8",
   authDomain: "hackathon-2022-b997c.firebaseapp.com",
+  databaseURL: "https://hackathon-2022-b997c-default-rtdb.firebaseio.com",
   projectId: "hackathon-2022-b997c",
   storageBucket: "hackathon-2022-b997c.appspot.com",
   messagingSenderId: "217569159200",
   appId: "1:217569159200:web:fded9663e1d4d46423b445",
-  measurementId: "G-VQ6H4JJRF5",
+  measurementId: "G-VQ6H4JJRF5"
 };
 
-firebase.initializeApp(firebaseConfig);
-
-export const auth = firebase.auth();
-
-export const db = firebase.firestore();
-
-export const storage = firebase.storage();
-
-export default firebase;
+const app = initializeApp(firebaseConfig);
